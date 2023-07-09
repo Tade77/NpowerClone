eed58b75530ffe3461f1abe0c5e6dbaa4a45a26d
Tade77 <adekunletaofeeq@mail.com>
2023-07-09 11:17:33 +0100

    Installing dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Container } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <Container>
       <div>
         <h1>Hello</h1>
       </div>
-    </>
+    </Container>
   );
 }
 
